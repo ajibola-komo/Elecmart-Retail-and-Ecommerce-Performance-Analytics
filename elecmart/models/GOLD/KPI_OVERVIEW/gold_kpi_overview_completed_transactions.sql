928b241c2c0e0a12a80fd46bf07997aaeb9c7ace
ajibola-komo <ajibolaokomolafe@gmail.com>
2026-04-06 23:43:10 +0100

    updated error in KPI

--- elecmart/models/GOLD/KPI_OVERVIEW/gold_kpi_overview_completed_transactions.sql
+++ elecmart/models/GOLD/KPI_OVERVIEW/gold_kpi_overview_completed_transactions.sql
@@ -17,7 +17,7 @@ round(coalesce((sum(transaction_total) - sum(transaction_cost)), 0), 2) as gross
 ), final as (
 select total_sales, total_cost, gross_profit, average_order_value, total_transactions, total_units_sold, 
 total_revenue_returned, total_cogs_returned, total_transactions_returned, total_units_returned, 
-gross_profit_returned, round(gross_profit / nullif(total_sales, 0) * 100, 2) as profit_margin  
+gross_profit_returned, round(gross_profit / nullif(total_sales, 0) * 100, 2) as profit_margin,
 round(total_revenue_returned/nullif(total_transactions_returned,0),2) as average_order_value_returned
 from additional_metrics cross join get_returned_metrics
 )
