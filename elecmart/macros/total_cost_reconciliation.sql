7c8b2af22e367c9ce40ab7a5c235da35b94a90c1
ajibola-komo <ajibolaokomolafe@gmail.com>
2026-04-22 13:35:48 +0200

    updated ERD diagram

--- elecmart/macros/total_cost_reconciliation.sql
+++
@@ -1,9 +0,0 @@
-{% test total_cost_matches_transactions(model) %}
-
-select *
-from {{ model }}
-where total_cogs != (
-    select coalesce(sum(line_cost), 0) from {{ ref('gold_fact_completed_transaction') }} where transaction_status = 'Completed'
-)
-
-{% endtest %}
